c8d0a24ded68a926d379a667d1e84cdce260716a
WcW <1754835+w121211@users.noreply.github.com>
2023-07-09 14:35:25 +0900

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+// For more information, see https://crawlee.dev/
+import { PlaywrightCrawler, ProxyConfiguration } from 'crawlee';
+import { router } from './routes.js';
+
+const startUrls = ['https://crawlee.dev'];
+
+const crawler = new PlaywrightCrawler({
+    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+    requestHandler: router,
+
+    // headless: false,
+});
+
+await crawler.run(startUrls);
